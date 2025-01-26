bde7fb1e9fd60cf3f06d8e1c5ab65d20e7616f10
unknown <est.hguillenp0201@uml.edu.ni>
2025-01-26 15:39:54 -0600

    se agrego la traduccion

--- src/components/ui/api.tsx
+++ src/components/ui/api.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-
+// Configuración de Axios con las variables de entorno
 const api = axios.create({
   baseURL: import.meta.env.VITE_BIBLE_API_URL,
   headers: {
@@ -8,23 +8,26 @@ const api = axios.create({
   },
 });
 
+// Obtención del ID de la Biblia desde las variables de entorno
+const reinaValeraId = import.meta.env.VITE_BIBLE_ID;
+
+// Funciones de la API
 export const getReinaValeraBooks = async () => {
-  const reinaValeraId = "592420522e16049f-01"; // ID de la Biblia Reina Valera 1960
   const response = await api.get(`/bibles/${reinaValeraId}/books`);
   return response.data;
 };
 
 export const getBookChapters = async (bookId: string) => {
-  const response = await api.get(`/bibles/de4e12af7f28f599-02/books/${bookId}/chapters`);
+  const response = await api.get(`/bibles/${reinaValeraId}/books/${bookId}/chapters`);
   return response.data;
 };
 
 export const getChapterVerses = async (chapterId: string) => {
-  const response = await api.get(`/bibles/de4e12af7f28f599-02/chapters/${chapterId}/verses`);
+  const response = await api.get(`/bibles/${reinaValeraId}/chapters/${chapterId}/verses`);
   return response.data;
 };
 
 export const getVerseText = async (verseId: string) => {
-  const response = await api.get(`/bibles/de4e12af7f28f599-02/verses/${verseId}`);
+  const response = await api.get(`/bibles/${reinaValeraId}/verses/${verseId}`);
   return response.data;
-};
+}
